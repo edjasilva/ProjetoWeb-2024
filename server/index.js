f1679d786e05882bd4ef9cb6166276102fd4cf2b
edjasilva <tamaramonteiro2014@icloud.com>
2024-06-03 23:02:43 +0100

    Testing controller updates.~

--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ import mapaRoutes from './routes/mapaRoutes.js';
 import homeRoutes from './routes/homeRoutes.js';
 import spotsRoutes from './routes/spotsRoutes.js';
 import dashboardRoutes from './routes/dashboardRoutes.js';
-import nonComSpots from './routes/nonComSpotsRoutes.js';
 import ping from './routes/pingRoutes.js';
 
 
@@ -54,7 +53,6 @@ server.use("/mapa", mapaRoutes);
 server.use("/", homeRoutes);
 server.use("/spots", spotsRoutes);
 server.use("/dashboard", dashboardRoutes);
-server.use("/nonCom", nonComSpots);
 server.use("/ping", ping);
 
 
